a42b2944e00c0dede2742f19376e5cf83ca4e221
Beatriz <rm5544044@fiap.com.br>
2023-10-27 04:50:21 -0300

    Adicionando exercicio sobre array de animals

---
+++ animals.js
@@ -0,0 +1,6 @@
+const animals = ["Beluga","Baleia","Narval","Elefante","Gato"];
+if (animals.includes("Leão")) {
+    console.log("Leão existe no array animals!!");
+} else {
+    console.log("Leão não existe no array animals :(");
+}
